379c47089bed350c96edbfc6a0c353cb44a3c79a
Race Mahoney <rjm5893@psu.edu>
2024-09-19 23:59:27 -0400

    fix: spelling

--- module4/front-end-development/src/components/Hero.js
+++ module4/front-end-development/src/components/Hero.js
@@ -5,12 +5,12 @@ import logo from "../assets/mountain.svg";
 const Hero = () => (
   <div className="text-center hero my-5">
     <img className="mb-3 app-logo" src={logo} alt="mountain logo" width="140"/>
-    <h1 className="mb-4">Particpate in a Lottery to Climb K2 in 2025!</h1>
+        <h1 className="mb-4">Participate in a Lottery to Climb K2 in 2025!</h1>
 
     <p className="lead">
             1. Log In
             <br></br>
-            2. Enter all revelent informaiton
+            2. Enter all relevant information
             <br></br>
             3.Wait
             <br></br>
